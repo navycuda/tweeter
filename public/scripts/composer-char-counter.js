ea9d0503e01e5162a1c34e58bdcca9d050e4c24f
NavyCuda <navysupra@gmail.com>
2022-08-25 23:26:05 -0700

    Refactor updateCounter()

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,30 +1,26 @@
-const updateCounter = (pos, counter) => {
-  
-  counter.val(pos);
-  if (pos < 0) {
-    if (!counter.hasClass('red')) {
-      counter.addClass('red');
-    }
-  } else {
-    if (counter.hasClass('red')) {
-      counter.removeClass('red');
-    }
-  }
-};
-
 $(() => {
   const _maxTweetLengthDoNotChange = 140;
   const counter = $('output.counter');
-  // counter.val(120);
-
   const tweetText = $('#tweet-text');
 
-  tweetText.on('input', () => {
+  const updateCounter = () => {
     const pos = _maxTweetLengthDoNotChange - tweetText.val().length;
-    updateCounter(pos, counter);
+    counter.val(pos);
+    if (pos < 0) {
+      if (!counter.hasClass('red')) {
+        counter.addClass('red');
+      }
+    } else {
+      if (counter.hasClass('red')) {
+        counter.removeClass('red');
+      }
+    }
+  };
+
+  tweetText.on('input', () => {
+    updateCounter();
   });
   tweetText.on('focus', () => {
-    const pos = _maxTweetLengthDoNotChange - tweetText.val().length;
-    updateCounter(pos, counter);
+    updateCounter();
   });
 });
